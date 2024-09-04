0847c2121ce90e3c589d6e31e2c94e55fe4d84cf
adrianmartins-cps <adrian.silva64@etec.sp.gov.br>
2024-09-04 20:40:21 -0300

    Aula 0904 Estrutura de Projewto React

---
+++ minideiablog/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <>
+        <footer className={style.footerooter}>
+            <h3>Ideias de quem gosta de ficar com pé na estrada!</h3>
+            <p>IdeiasApp &copy; 2024</p>
+        </footer>
+        
+        </>
+    )
+}
