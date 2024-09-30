51813b1b8bdd4f4da247ed4e7f8cb344417d3d9d
adrianmartins-cps <adrian.silva64@etec.sp.gov>
2024-09-30 20:43:00 -0300

    Aula0930 -Authenticação no Firebase e Rotas

--- minideiablog/src/App.jsx
+++ minideiablog/src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
+import { BrowserRouter, Routes, Router, Navigate, Form} from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import Login from './pages/Login/Login'
+import Home from './pages/Home/Home'
+import CreatePost from './pages/CreatPost/CreatePost'
 function App() {
   return (
     <>
       <div>
+        <BrowserRouter>
         <NavBar />
+        <div classNmae='container' >
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/Login' element={<Login />}></Route>
+            <Route path='/post/create' element={<CreatePost />}></Route>        </Routes>
+        </div>
         <Footer />
-        
+        </BrowserRouter>
       </div>
     </>
   )
