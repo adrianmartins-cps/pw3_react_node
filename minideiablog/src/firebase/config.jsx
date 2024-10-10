a58fef0d1538ee9f27b4829272b24b762771a5f4
adrianmartins-cps <adrian.silva64@etec.sp.gov.brgit config --global user.email adrian.silva64@etec.sp.gov>
2024-10-10 19:14:13 -0300

    Ajustes

--- minideiablog/src/firebase/config.jsx
+++ minideiablog/src/firebase/config.jsx
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
 import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
-  apiKey: "AIzaSyCzvetyzpLnlV6-4duZfRuskmiDnEBcHMU",
-  authDomain: "miniblog-icoma.firebaseapp.com",
-  projectId: "miniblog-icoma",
-  storageBucket: "miniblog-icoma.appspot.com",
-  messagingSenderId: "643979843568",
-  appId: "1:643979843568:web:94507c223cddc1cd20ef35",
-  measurementId: "G-005V8EV8BX"
-}
-const app = initializeApp(firebaseConfig)
+  apiKey: "AIzaSyDG36ATwDx0Uu3UEqz0LBWXNHjj2cAOUqw",
+  authDomain: "miniblog-adriana.firebaseapp.com",
+  projectId: "miniblog-adriana",
+  storageBucket: "miniblog-adriana.appspot.com",
+  messagingSenderId: "412338052677",
+  appId: "1:412338052677:web:3693a78b06220dd8094d2c",
+  measurementId: "G-9Z2ZBENVSQ"
+};
+pp = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
 const db = getFirestore(app)
 
